4104a72642ce381e618f4e5a33809ced0f7e6a90
raulnavarro <raul.navarro@corusconsulting.com>
2023-09-21 19:34:46 +0200

    completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer({ storage: storage });
 
 var app = express();
 
@@ -9,6 +11,10 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
 });
 
 
